0c7792e7de40a243e51ca678bbde24e51222246f
jensoliver <jojo@jbothe.de>
2017-01-10 17:05:07 +0100

    Added Copyright

--- ZZZIPCheck.pm
+++ ZZZIPCheck.pm
@@ -1,3 +1,10 @@
+# --
+# Copyright (C) 2017 by Jens Bothe
+# --
+# This software comes with ABSOLUTELY NO WARRANTY.
+#
+# Copy this file to /opt/otrs/Kernel/Config/Files
+# 
 $Self->{'Ticket::Frontend::ZoomCollectMetaFilters'}->{'IP-Blacklist'} =  {
   'Meta' => {
     'Name' => 'IP-Blacklist',
